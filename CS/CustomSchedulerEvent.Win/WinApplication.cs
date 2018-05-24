24fbc490c9b6f21b869966aa4e1f7497bd78e397
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2018-05-24 14:28:22 +0300

    add missing code

---
+++ CS/CustomSchedulerEvent.Win/WinApplication.cs
@@ -0,0 +1,23 @@
+using DevExpress.ExpressApp.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp;
+
+namespace CustomSchedulerEvent.Win {
+    public partial class CustomSchedulerEventWindowsFormsApplication : WinApplication {
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            args.ObjectSpaceProvider = new XPObjectSpaceProvider(args.ConnectionString, args.Connection);
+        }
+        public CustomSchedulerEventWindowsFormsApplication() {
+            InitializeComponent();
+        }
+
+        private void CustomSchedulerEventWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+    }
+}
