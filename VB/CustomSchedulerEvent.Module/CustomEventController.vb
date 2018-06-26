b9100e6a2ab2e48c4059cb9f8123271a1cc0da62
DevExpressExampleBot <fakeEmail>
2018-06-26 18:08:29 +0300

    Source auto update [skip ci]

---
+++ VB/CustomSchedulerEvent.Module/CustomEventController.vb
@@ -0,0 +1,45 @@
+﻿Imports System
+Imports System.ComponentModel
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Text
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.Persistent.Base
+Imports DevExpress.ExpressApp.Scheduler
+Imports DevExpress.Persistent.Validation
+
+Namespace CustomSchedulerEvent.Module
+    Partial Public Class CustomEventController
+        Inherits ViewController
+
+        Private schedulerEditor As SchedulerListEditorBase
+
+        Public Sub New()
+            InitializeComponent()
+            RegisterActions(components)
+            Me.TargetViewType = ViewType.ListView
+        End Sub
+
+        Private Sub CustomEventController_Activated(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Activated
+            schedulerEditor = TryCast(CType(View, ListView).Editor, SchedulerListEditorBase)
+            If schedulerEditor IsNot Nothing Then
+                AddHandler schedulerEditor.ExceptionEventCreated, AddressOf schedulerEditor_ExceptionEventCreated
+            End If
+        End Sub
+
+        Private Sub CustomEventController_Deactivated(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Deactivated
+            If schedulerEditor IsNot Nothing Then
+                RemoveHandler schedulerEditor.ExceptionEventCreated, AddressOf schedulerEditor_ExceptionEventCreated
+            End If
+        End Sub
+
+        Private Sub schedulerEditor_ExceptionEventCreated(ByVal sender As Object, ByVal e As ExceptionEventCreatedEventArgs)
+            If TypeOf e.PatternEvent Is ExtendedEvent AndAlso TypeOf e.ExceptionEvent Is ExtendedEvent Then
+                CType(e.ExceptionEvent, ExtendedEvent).Notes = CType(e.PatternEvent, ExtendedEvent).Notes
+                Validator.RuleSet.Validate(ObjectSpace, e.ExceptionEvent, "SchedulerValidation")
+            End If
+        End Sub
+    End Class
+End Namespace
---
+++ VB/CustomSchedulerEvent.Win/WinApplication.vb
@@ -0,0 +1,25 @@
+﻿Imports DevExpress.ExpressApp.Xpo
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.Win
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp
+
+Namespace CustomSchedulerEvent.Win
+    Partial Public Class CustomSchedulerEventWindowsFormsApplication
+        Inherits WinApplication
+
+        Protected Overrides Sub CreateDefaultObjectSpaceProvider(ByVal args As CreateCustomObjectSpaceProviderEventArgs)
+            args.ObjectSpaceProvider = New XPObjectSpaceProvider(args.ConnectionString, args.Connection)
+        End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub CustomSchedulerEventWindowsFormsApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles Me.DatabaseVersionMismatch
+            e.Updater.Update()
+            e.Handled = True
+        End Sub
+    End Class
+End Namespace
